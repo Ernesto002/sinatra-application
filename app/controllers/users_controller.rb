3e48de5b07b81a2ef87d0be05d095e02ff5055cf
ernesto <ernesto002@icloud.com>
2021-02-28 21:47:58 -0800

    create user mvc

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+  get "/signup" do
+    erb :"/users/new.html"
+  end
+
+  get "/login" do
+    erb :"/users/login.html"
+  end
+
+  post "/login" do
+    #session[:user_id] = user.id
+    redirect "/users/:id"
+  end
+
+  post "/logout" do 
+    #session.destroy
+    redirect "/"
+  end
+
+  # shows all reviews by user
+  get "users/:id" do 
+    erb :"/users/show.html"
+  end
+
+  # create new user
+  post "users/new" do 
+    # create new user from params
+    # send validation
+    redirect "/login" 
+  end
+
+end
