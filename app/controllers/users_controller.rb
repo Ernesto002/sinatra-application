3fcb351af02c465c7e6d01ddff03af16be170538
ernesto <ernesto002@icloud.com>
2021-02-28 22:50:45 -0800

    lint, resolve needs_migration? error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
 
   post "/login" do
     user = User.find_by(username: params[:username])
-    redirect "/signup" unless user && user.authenticate(params[:password]) session[:user_id] = user.id
+    redirect "/signup" unless user && user.authenticate(params[:password])
+    session[:user_id] = user.id
     redirect "/users/#{user.id}"
   end
 
